fc3c5e6160179f72c0089d26a7141b5b09656b69
hyone <hyone.development@gmail.com>
2014-11-10 11:34:04 +0900

    Support CentOS 7

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'build-essential'
 depends          'git'
+
+supports         'ubuntu', '12.04'
+supports         'centos', '>= 6'
